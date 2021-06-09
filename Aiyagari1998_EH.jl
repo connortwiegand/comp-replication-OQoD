2b976113771d7275f32532c2b5d2498f613dc88c
efhart4 <84650748+efhart4@users.noreply.github.com>
2021-06-09 14:18:09 -0700

    Add files via upload

--- Aiyagari1998_EH.jl
+++ Aiyagari1998_EH.jl
@@ -2,13 +2,19 @@
 using BasisMatrices,LinearAlgebra,Parameters,Optim,QuantEcon,DataFrames,Gadfly,SparseArrays,Arpack
 using Roots
 
+#=
+ 1. added leisure to utility function   U(HH::HHModel,c)
+ 2. budget constraint                   setupgrids_shocks!(HH::HHModel, curv=1.7)
+ 3. Value function with  growth         setupgrids_shocks!(HH::HHModel, curv=1.7)
+ =#
 
-#                                          Setup: HH struct and Utility function
+# structure and utility
 #------------------------------------------------------------------------------------------------
-    # This setup is referenced in Aiyagari setup
+
     @with_kw mutable struct HHModel
         #Preference Parameters
         γ::Float64 = 1. #Risk aversion
+        η::Float64 = 0.328 # consumption elasticity?            Evan: June 9
         β::Float64 = 0.985 #Discount Rate
 
         #Prices
@@ -31,31 +37,66 @@ using Roots
         k::Int = 2 #type of interpolation
         Vf::Vector{Interpoland} = Interpoland[]
         cf::Vector{Interpoland} = Interpoland[]
+        lf::Vector{Interpoland} = Interpoland[]
 
         #Extra
         EΦ′::SparseMatrixCSC{Float64,Int64} = spzeros(0,0)
         Φ::SparseMatrixCSC{Float64,Int64} = spzeros(0,0)
+
+        #additional parameters without a Home
+        χ::Float64 = 0.9923
+        g::Float64 = 0.9923
+    end
+
+
+    @with_kw mutable struct AiyagariModel
+        HH::HHModel = HHModel()
+
+        #Production Parameters
+        α::Float64 = 0.3
+        δ::Float64 = 0.025
+        Θ̄::Float64 = 1.
+
+        #Moments to match/prices
+        W̄::Float64 = 1.
+        R̄::Float64 = 1.01
+        K2Y::Float64 = 10.2 #capital to output ratio
+        N̄::Float64 = 1.
+
+        #Distribution Parameters
+        Ia::Int = 1000 #Number of gridpoints for distribution
+        z̄::Matrix{Float64} = zeros(0,0) #Gridpoints for the state variables
+        ω̄::Vector{Float64} = zeros(0) #Fraction of agents at each grid level
+        H::SparseMatrixCSC{Float64,Int64} = spzeros(Ia,Ia) #Transition matrix
     end
 
     """
         U(HH::HHModel,c)
     """
-    function U(HH::HHModel,c)
+    function U(HH::HHModel,c,l)
         γ = HH.γ
+        η = HH.η
         if γ == 1
-            return log.(c)
+            return log.(c.^(η) .* l.^(1-η))
         else
-            return (c.^(1-γ))./(1-γ)
+            return ((c.^(η) .* l.^(1-η)).^(1-γ))./(1-γ)
         end
     end
+#------------------------------------------------------------------------------------------------
 
+
+
+# shocks
+#------------------------------------------------------------------------------------------------
     """
         setupgrids_shocks!(HH::HHModel, curv=1.7)
     Set up non-linear grids for interpolation
     """
     function setupgrids_shocks!(HH::HHModel, curv=1.7)
-        @unpack a̲,a̅,Na,ρ_ϵ,σ_ϵ,Nϵ,k,r̄,w̄,β = HH
+        @unpack a̲,a̅,Na,ρ_ϵ,σ_ϵ,Nϵ,k,r̄,w̄,β,χ,γ,η,g = HH
+        
         #Compute grid on A
+        # grid over l ? 
         agrid = (a̅-a̲).*LinRange(0,1,Na).^curv .+ a̲
 
         #Store markov chain
@@ -69,18 +110,56 @@ using Roots
 
         Vf = HH.Vf = Vector{Interpoland}(undef,Nϵ)
         cf = HH.cf = Vector{Interpoland}(undef,Nϵ)
+        lf = HH.cf = Vector{Interpoland}(undef,Nϵ)
         for s in 1:Nϵ
-            c = @. r̄*a + w̄*HH.ϵ[s]
-            V = U(HH,c)./(1-β)
+            #------------------------------------------------------------------------------------------------------------------------ 
+            l = 0.5 # fixed l until we get that figured out. 
+            c = @. r̄*a + (1-l)*w̄*HH.ϵ[s] + χ            # Evan June 6
+                #= 
+                Im a bit confused about this, what about saving for next period?
+                This is a budget constraint right hand side
+            =#
+            if γ == 1
+                V = U(HH,c,l)./(1-β)
+            else
+                V = U(HH,c,l)./(1-(β(1+g)^[η(1-γ)] ))
+            end
+            # V = U(HH,c,l)./(1-β)
+                #= 
+                infinite sum  ∑(β(1+g)^[η(1-μ)] )^t  replaces ∑ β^t
+                setting μ to 1 gave us a log utility function. It also cancels growth out of our value funciton
+            =#
+            #------------------------------------------------------------------------------------------------------------------------ 
 
             Vf[s]= Interpoland(abasis,V)
             cf[s]= Interpoland(abasis,c)
+            lf[s]= Interpoland(abasis,l)
         end
 
         #Expectations of 1st derivative of Basis functions
         HH.EΦ′ = kron(Π,BasisMatrix(abasis,Direct(),nodes(abasis)[1],[1]).vals[1])
         HH.Φ = kron(Matrix{Float64}(I,Nϵ,Nϵ),BasisMatrix(abasis,Direct()).vals[1])
     end
+
+
+    """
+        setupgrids_shocks!(AM::AiyagariModel)
+    Setup the grids and shocks for the aiyagari model
+    """
+    function setupgrids_shocks!(AM::AiyagariModel,curv=2.)
+        @unpack HH,Ia,N̄= AM
+        @unpack a̲,a̅,Nϵ = HH
+        setupgrids_shocks!(HH)
+        #Normalize so that average labor supply is 1
+        πstat = real(eigs(HH.Π',nev=1)[2])
+        πstat ./= sum(πstat)
+        HH.ϵ = HH.ϵ./dot(πstat,HH.ϵ)*N̄
+        #Grid for distribution
+        agrid = (a̅-a̲).*LinRange(0,1,Ia).^curv .+ a̲
+        AM.z̄ = hcat(kron(ones(Nϵ),agrid),kron(1:Nϵ,ones(Ia)))
+        AM.ω̄ = ones(Ia*Nϵ)/(Ia*Nϵ)
+    end
+
 #------------------------------------------------------------------------------------------------------------------------ 
 
 
@@ -96,28 +175,28 @@ using Roots
     Iterates on Euler equation using endogenous grid method
     """
     function iterate_endogenousgrid(HH,a′grid,cf′)
-    @unpack γ,ϵ,β,Nϵ,Π,r̄,w̄,a̲= HH
-    c′ = zeros(length(a′grid),Nϵ)
-    for s in 1:Nϵ
-        c′[:,s]= cf′[s](a′grid)
-    end
-
-    EERHS = β*(1+r̄)*(c′).^(-γ)*Π' #RHS of Euler Equation
-    c = EERHS.^(-1/γ)
-
-    #compute implies assets
-    a = ((c .+ a′grid) .- w̄ .*ϵ')./(1+r̄)
-
-    cf = Vector{Interpoland}(undef,Nϵ)
-    for s in 1:Nϵ
-        if a[1,s]> a̲
-            c̲ = r̄*a̲ + w̄*ϵ[s]
-            cf[s]= Interpoland(Basis(SplineParams([a̲; a[:,s]],0,1)),[c̲;c[:,s]])
-        else
-            cf[s]= Interpoland(Basis(SplineParams(a[:,s],0,1)),c[:,s])
+        @unpack γ,ϵ,β,Nϵ,Π,r̄,w̄,a̲= HH
+        c′ = zeros(length(a′grid),Nϵ)
+        for s in 1:Nϵ
+            c′[:,s]= cf′[s](a′grid)
         end
-    end
-    return cf
+
+        EERHS = β*(1+r̄)*(c′).^(-γ)*Π' #RHS of Euler Equation
+        c = EERHS.^(-1/γ)
+
+        #compute implies assets
+        a = ((c .+ a′grid) .- w̄ .*ϵ')./(1+r̄)
+
+        cf = Vector{Interpoland}(undef,Nϵ)
+        for s in 1:Nϵ
+            if a[1,s]> a̲
+                c̲ = r̄*a̲ + w̄*ϵ[s]
+                cf[s]= Interpoland(Basis(SplineParams([a̲; a[:,s]],0,1)),[c̲;c[:,s]])
+            else
+                cf[s]= Interpoland(Basis(SplineParams(a[:,s],0,1)),c[:,s])
+            end
+        end
+        return cf
     end
 
 
@@ -136,9 +215,10 @@ using Roots
         cf′ = HH.cf
     end
     end
+#------------------------------------------------------------------------------------------------
 
-
-
+# junk
+#------------------------------------------------------------------------------------------------
     # only implemented for HH control below not for Aiyagari
     """
     ee_errors(HH,agrid) 
@@ -169,46 +249,8 @@ using Roots
 
 # Main course
 
-    # Functions and structure
-    @with_kw mutable struct AiyagariModel
-        HH::HHModel = HHModel()
-
-        #Production Parameters
-        α::Float64 = 0.3
-        δ::Float64 = 0.025
-        Θ̄::Float64 = 1.
-
-        #Moments to match/prices
-        W̄::Float64 = 1.
-        R̄::Float64 = 1.01
-        K2Y::Float64 = 10.2 #capital to output ratio
-        N̄::Float64 = 1.
-
-        #Distribution Parameters
-        Ia::Int = 1000 #Number of gridpoints for distribution
-        z̄::Matrix{Float64} = zeros(0,0) #Gridpoints for the state variables
-        ω̄::Vector{Float64} = zeros(0) #Fraction of agents at each grid level
-        H::SparseMatrixCSC{Float64,Int64} = spzeros(Ia,Ia) #Transition matrix
-    end
-
-    """
-        setupgrids_shocks!(AM::AiyagariModel)
-    Setup the grids and shocks for the aiyagari model
-    """
-    function setupgrids_shocks!(AM::AiyagariModel,curv=2.)
-        @unpack HH,Ia,N̄= AM
-        @unpack a̲,a̅,Nϵ = HH
-        setupgrids_shocks!(HH)
-        #Normalize so that average labor supply is 1
-        πstat = real(eigs(HH.Π',nev=1)[2])
-        πstat ./= sum(πstat)
-        HH.ϵ = HH.ϵ./dot(πstat,HH.ϵ)*N̄
-        #Grid for distribution
-        agrid = (a̅-a̲).*LinRange(0,1,Ia).^curv .+ a̲
-        AM.z̄ = hcat(kron(ones(Nϵ),agrid),kron(1:Nϵ,ones(Ia)))
-        AM.ω̄ = ones(Ia*Nϵ)/(Ia*Nϵ)
-    end
-
+# find stationary distribution and calibratesteadystate
+#------------------------------------------------------------------------------------------------
     """
         find_stationarydistribution!(AM::AiyagariModel,V)
     Computes the stationary distribution 
@@ -235,6 +277,32 @@ using Roots
         AM.ω̄ ./= sum(AM.ω̄) 
     end
 
+    function calibratesteadystate!(AM)
+        @unpack Θ̄,α,N̄,K2Y,R̄ = AM
+        AM.HH.r̄ = R̄ - 1
+        Y2K = 1/K2Y
+        AM.δ = α*Y2K + 1 - R̄ #matching capital to output ratio and interest rate gives depreciation rate
+        K2N = (Y2K/Θ̄)^(1/(α-1)) #relationship between capital to output and capital to labor
+        K̄ = K2N*N̄
+        AM.W̄ = AM.HH.w̄ = (1-α)*Θ̄*K2N^α
+    
+        setupgrids_shocks!(AM)
+        function βres(β)
+            AM.HH.β=β
+            # solvebellman!(AM.HH)      # collocation
+            solveHHproblem_eg!(AM.HH)   # Endogenopus grid
+            find_stationarydistribution!(AM)
+            return dot(AM.ω̄,AM.z̄[:,1]) -K̄
+        end
+    
+        Q̄ = 1/R̄
+        fzero(βres,Q̄^2,Q̄^1.2)
+    end
+    
+#------------------------------------------------------------------------------------------------
+
+# interest determination
+#------------------------------------------------------------------------------------------------    
     function capital_supply_demand(AM,R)
         @unpack Θ̄,α,N̄,δ = AM
         AM.R̄ = R
@@ -258,55 +326,37 @@ using Roots
 # 
 
 
-AM = AiyagariModel()
-AM.HH.β = 0.99
-setupgrids_shocks!(AM)
-
-
-
-
-# Figure 1: capital steady state: 
-Rgrid = LinRange(1.,1.007,10)
-KSKD = hcat([capital_supply_demand(AM,R) for R in Rgrid]...)
-plot(layer(y=Rgrid,x=KSKD[1,:],color=["Capital Supplied"],Geom.line),
-    layer(y=Rgrid,x=KSKD[2,:],color=["Capital Demanded"],Geom.line),
-    Guide.ylabel("Gross Interest Rate"), Guide.xlabel("Capital"))
-
-
-# 
-
-
-
-# this is a bit out of the blue
-
-AM.R̄ = 1.01 #target a quarterly interest rate of 1%
-
-
-# one last function
-function calibratesteadystate!(AM)
-    @unpack Θ̄,α,N̄,K2Y,R̄ = AM
-    AM.HH.r̄ = R̄ - 1
-    Y2K = 1/K2Y
-    AM.δ = α*Y2K + 1 - R̄ #matching capital to output ratio and interest rate gives depreciation rate
-    K2N = (Y2K/Θ̄)^(1/(α-1)) #relationship between capital to output and capital to labor
-    K̄ = K2N*N̄
-    AM.W̄ = AM.HH.w̄ = (1-α)*Θ̄*K2N^α
-
+# run functions
+#------------------------------------------------------------------------------------------------  
+    AM = AiyagariModel()
+    AM.HH.β = 0.99
     setupgrids_shocks!(AM)
-    function βres(β)
-        AM.HH.β=β
-        # solvebellman!(AM.HH)      # collocation
-        solveHHproblem_eg!(AM.HH)   # Endogenopus grid
-        find_stationarydistribution!(AM)
-        return dot(AM.ω̄,AM.z̄[:,1]) -K̄
-    end
 
-    Q̄ = 1/R̄
-    fzero(βres,Q̄^2,Q̄^1.2)
-end
 
-AM = AiyagariModel()
 
-calibratesteadystate!(AM)
 
-plot(x=AM.z̄[AM.z̄[:,1].>1.,1],y=AM.ω̄[AM.z̄[:,1].>1.],Geom.bar)
+    # Figure 1: capital steady state: 
+    Rgrid = LinRange(1.,1.007,10)
+    KSKD = hcat([capital_supply_demand(AM,R) for R in Rgrid]...)
+    plot(layer(y=Rgrid,x=KSKD[1,:],color=["Capital Supplied"],Geom.line),
+        layer(y=Rgrid,x=KSKD[2,:],color=["Capital Demanded"],Geom.line),
+        Guide.ylabel("Gross Interest Rate"), Guide.xlabel("Capital"))
+
+
+    # 
+
+
+
+    # this is a bit out of the blue
+
+    AM.R̄ = 1.01 #target a quarterly interest rate of 1%
+
+
+    # one last function
+
+    AM = AiyagariModel()
+
+    calibratesteadystate!(AM)
+
+    plot(x=AM.z̄[AM.z̄[:,1].>1.,1],y=AM.ω̄[AM.z̄[:,1].>1.],Geom.bar)
+#------------------------------------------------------------------------------------------------  
